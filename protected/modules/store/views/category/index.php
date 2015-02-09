537342eb0670fc06f99c5eab80734b42c31446e9
Lamoid <wdb@mail.ru>
2015-02-09 16:02:18 +0500

    category view

--- protected/modules/store/views/category/index.php
+++ protected/modules/store/views/category/index.php
@@ -11,7 +11,56 @@
  */
  
 ?>
+<div class="banner">
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+			<!--<li data-target="#carousel-example-generic" data-slide-to="2"></li>-->
+		</ol>
 
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<img src="<?php echo Yii::app()->theme->baseUrl ?>/assets/images/banner/banner2.jpg" alt="...">
+				<div class="carousel-caption">
+					В честь торжественного открытия нашего магазина <a href="#">скидка на ножи 20%</a>
+				</div>
+			</div>
+			<div class="item">
+				<img src="<?php echo Yii::app()->theme->baseUrl ?>/assets/images/banner/banner.gif" alt="...">
+				<div class="carousel-caption">
+					В честь торжественного открытия нашего магазина <a href="#">скидка на ножи 20%</a>
+				</div>
+			</div>
+			<div class="item">
+				<img src="<?php echo Yii::app()->theme->baseUrl ?>/assets/images/banner/banner3.jpg" alt="...">
+				<div class="carousel-caption">
+					В честь торжественного открытия нашего магазина <a href="#">скидка на ножи 20%</a>
+				</div>
+			</div>
+			<div class="item">
+				<img src="<?php echo Yii::app()->theme->baseUrl ?>/assets/images/banner/banner4.jpg" alt="...">
+				<div class="carousel-caption">
+					В честь торжественного открытия нашего магазина <a href="#">скидка на ножи 20%</a>
+				</div>
+			</div>
+		</div>
+
+		<!-- Controls -->
+		<!--<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">-->
+			<!--<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>-->
+			<!--<span class="sr-only">Previous</span>-->
+		<!--</a>-->
+		<!--<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">-->
+			<!--<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>-->
+			<!--<span class="sr-only">Next</span>-->
+		<!--</a>-->
+	</div> <!-- carousel -->
+</div>
 <div class="category">
 <?php
 
