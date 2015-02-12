2d799e96e4d34e88e4f20ea57721b8582885c5a8
Lamoid <wdb@mail.ru>
2015-02-13 01:14:12 +0500

    rules form validate

--- protected/modules/orders/views/cart/_steps/contacts.php
+++ protected/modules/orders/views/cart/_steps/contacts.php
@@ -37,49 +37,32 @@
     	    phone_number = phone_number.replace(/\s+/g, "");
     	    return this.optional(element) || phone_number.length > 9 && 
     	    phone_number.match(/^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/);
-    	}, "<br />Please specify a valid phone number");
+    	}, "<br />Введите правильный номер телефона");
 		
 		
 		$('button').on('click', function() {
 			var form = $( "#cartId" );
 			
-			 $.validator.setDefaults({
+			i=0;
+			
+			$.validator.setDefaults({
 				ignore: []
 			});
 			
 			form.validate({
-				debug: true,
 				rules: {
 					'OrderCreateForm[phone]': {
 						required: true,
-						phoneUS: true
+						phoneno: true
 					},
 					'OrderCreateForm[email]': {
 						required: true,
 						email: true
 					},
-					username: {
+					'OrderCreateForm[user_city]': {
 						required: true,
 						minlength: 2
-					},
-					password: {
-						required: true,
-						minlength: 5
-					},
-					confirm_password: {
-						required: true,
-						minlength: 5,
-						equalTo: "#password"
-					},
-					email: {
-						required: true,
-						email: true
-					},
-					topic: {
-						required: "#newsletter:checked",
-						minlength: 2
-					},
-					agree: "required"
+					}
 				},
 				messages:{
 					'OrderCreateForm[phone]':{
@@ -87,31 +70,43 @@
 						minlength: "Логин должен быть минимум 4 символа",
 						maxlength: "Максимальное число символо - 16",
 					},
-
 					'OrderCreateForm[email]':{
+						required: "Это поле обязательно для заполнения",
+						minlength: "Email должен быть минимум 6 символа",
+						maxlength: "Email должен быть максимум 16 символов",
+					},
+					'OrderCreateForm[user_city]':{
 						required: "Это поле обязательно для заполнения",
 						minlength: "Пароль должен быть минимум 6 символа",
 						maxlength: "Пароль должен быть максимум 16 символов",
 					},
 				},
-				 highlight: function(element, errorClass, validClass) {
+				highlight: function(element, errorClass, validClass) {
+					i++;
 					
-					console.log(element);
 					
-					//$(element).find('fieldset').addClass(errorClass).removeClass(validClass);
-					//$(element.form).find("label[for=" + element.id + "]").addClass(errorClass);
-					$(element).closest('fieldset').collapse('show');
-					console.log($(element).closest('fieldset'));
-				},
-				showErrors: function (errorMap, errorList) {
+					console.log($(element).closest('div.collapse').eq(0));
+					if(i==1) $(element).closest('div.collapse').eq(0).collapse('show');
+					
+					// показываем только один error элемент
+					/*$(document).on('show.bs.collapse', function (e) {
+						//e.preventDefault();
+						i++;
+					});*/
+					
+					console.log(i);
+					
+				}
+				/*showErrors: function (errorMap, errorList) {
 					console.log(errorList);
-					console.log(errorMap);
+					//console.log(errorMap);
 					if (errorList.length) {
 						$("span").html(errorList[0].message);
 					}
-				}
+				}*/
 			});
-			if(!form.valid()) return false;;
+			//if(!form.valid()) return false;
+			console.log(form.valid());
 		});
 	});
 </script>
